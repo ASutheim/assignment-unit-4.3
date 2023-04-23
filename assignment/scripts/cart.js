a22e3da1d080fe1d52fe7fe550924814bc5f32f6
Anna Sutheim <asutheim@gmail.com>
2023-04-23 13:48:08 -0500

    Completed cart problem

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -33,3 +33,23 @@ listItems(basket);
 /*The code above creates a function called 'listItems' 
 which loops over the array 'basket' and console logs each 
 item in the basket.*/
+
+/*
+function empty(basket) {
+  for (let i = 0; i < basket.length; i++) {
+    basket.shift[i];
+  }
+
+  Leaving this non-functional code in here because I would love
+  to know why it wasn't working!
+*/
+
+function empty() {
+  basket = [];
+}
+
+console.log("Test of 'empty' function: should return empty array brackets:");
+empty();
+console.log(basket);
+
+/* The above code resets the array 'basket' to an empty array*/
