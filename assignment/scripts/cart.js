f65d115cf6b3a93b734da5840776f4d0669035fb
Anna Sutheim <asutheim@gmail.com>
2023-04-23 12:52:20 -0500

    Finished first part of cart project

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,16 @@
-console.log('***** Cart Functions *****');
+console.log("***** Cart Functions *****");
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+let basket = [];
+console.log("Test basket variable: should be an empty array:", basket);
+
+function addItem(item) {
+  basket.push(item);
+  return true;
+}
+
+console.log(
+  "Test of add.Item function: should return 'true'",
+  addItem("oranges")
+);
+console.log("Array 'basket' should now contain 'oranges':", basket);
